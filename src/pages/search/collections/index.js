40ffdc09c243c9f0e4dce1dcf7e36b596bb3fe18
(hanwool) <(hanwe92@gmail.com)>
2022-08-08 07:07:46 +0900

    기존 파일 커밋

---
+++ src/pages/search/collections/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export default function Collections () {
+    return(
+        <Container>
+            <h1>Collections</h1>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
