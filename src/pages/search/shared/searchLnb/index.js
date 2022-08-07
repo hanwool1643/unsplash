08b2044362db8d27f53345857c116bf5a7621888
(hanwool) <(hanwe92@gmail.com)>
2022-08-08 07:16:42 +0900

    feat: SearchLnb 컴포넌트 생성

---
+++ src/pages/search/shared/searchLnb/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function SearchLnb() {
+  return (
+    <Container>
+      SearchLnb
+    </Container>
+  );
+}
+
+const Container = styled.div`
+
+`;
