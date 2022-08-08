1b8da0026bcb1405a1a1231faa6aac79869ccb45
(hanwool) <(hanwe92@gmail.com)>
2022-08-08 18:55:19 +0900

    [WU-3] feat: SearchLnb 탭 menus 생성하여 현재 페이지 UI 기능 추가

--- src/pages/search/shared/searchLnb/index.js
+++ src/pages/search/shared/searchLnb/index.js
@@ -1,49 +1,88 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, useParams } from 'react-router-dom';
-import { IconCollection, IconImage, IconUser } from '../../../../incons';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { map } from 'lodash-es';
+import classNames from 'classnames';
+import {
+  ColorFilter, IconCollection, IconImage, IconUser,
+} from '../../../../incons';
 
 export default function SearchLnb() {
   const { query } = useParams();
+  const { pathname } = useLocation();
+
+  const menus = [
+    {
+      to: `/search/photos/${query}`,
+      icon: <IconImage />,
+      title: 'Photos',
+      count: '10K',
+      isActive: pathname.startsWith('/search/photos'),
+    },
+    {
+      to: `/search/collections/${query}`,
+      icon: <IconCollection />,
+      title: 'collections',
+      count: '3.9K',
+      isActive: pathname.startsWith('/search/collections'),
+    },
+    {
+      to: `/search/users/${query}`,
+      icon: <IconUser />,
+      title: 'user',
+      count: '10K',
+      isActive: pathname.startsWith('/search/users'),
+    },
+  ];
   return (
     <Container>
       <Nav>
-        <Link to={`/search/photos/${query}`}>
-          <IconImage />
-          <div className="title">Photos</div>
-          <div className="count">10K</div>
-        </Link>
-        <Link to={`/search/collections/${query}`}>
-          <IconCollection />
-          <div className="title">Collections</div>
-          <div className="count">3.9K</div>
-        </Link>
-        <Link to={`/search/users/${query}`}>
-          <IconUser />
-          <div className="title">Users</div>
-          <div className="count">10K</div>
-        </Link>
+        {
+                    map(menus, ({
+                      to, icon, title, count, isActive,
+                    }) => (
+                      <Link
+                        to={to}
+                        className={classNames({ isActive })}
+                      >
+                        {icon}
+                        <div className="title">{title}</div>
+                        <div className="count">{count}</div>
+                      </Link>
+                    ))
+                }
       </Nav>
+      <FilterBox>
+        <ColorFilter />
+      </FilterBox>
     </Container>
   );
 }
 
 const Container = styled.div`
-  padding: 0 20px;
-  font-size: 14px;
-  color: #767676;
-  text-transform: capitalize;
+  margin: 0 20px;
+  display: flex;
+  align-items: center;
+  
 `;
+
 const Nav = styled.div`
   display: flex;
-  & + &:first-child {
-    padding-left: 32px;
-  }
   a {
+    font-size: 14px;
+    color: #767676;
+    text-transform: capitalize;
     display: flex;
     align-items: center;
     height: 56px;
-    margin-left: 32px;
+    margin: 0 20px;
+    border: 2px solid transparent;
+    &:first-child{
+      margin-left: 0;
+    }
+    &.isActive{
+      border-bottom: 2px solid #333;
+    }
     svg {
       width: 18px;
       height: 18px;
@@ -52,6 +91,19 @@ const Nav = styled.div`
     .title {
       margin-right: 10px;
     }
-    
+  }
+`;
+const FilterBox = styled.div`
+  height: 48px;
+  width:100%;
+  display: flex;
+  align-items: center;
+  
+  svg {
+    color: #767676;
+    width: 18px;
+    height: 18px;
+    margin-left: auto;
+
   }
 `;
