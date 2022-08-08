6bba45749575af2ffa4640578af52150131c743b
(hanwool) <(hanwe92@gmail.com)>
2022-08-08 18:53:53 +0900

    [WU-3] feat: ContentContainer 페이지에 적용

--- src/pages/search/photos/index.js
+++ src/pages/search/photos/index.js
@@ -3,19 +3,20 @@ import styled from 'styled-components';
 
 import { useSearchPhotos } from './hooks/useSearchPhotos';
 import MasonryList from '../../../shared/list/MasonryList';
+import { ContentContainer } from '../../../shared/layout/Layout.Styled';
 
 export default function Photos() {
   const { data, isLoading } = useSearchPhotos();
   if (isLoading) return <p>Loading...</p>;
   return (
     <Container>
-      <MasonryList data={data.results} />
+      <ContentContainer>
+        <MasonryList data={data.results} />
+      </ContentContainer>
     </Container>
   );
 }
 
 const Container = styled.div`
-  box-sizing: border-box;
-  max-width: 1000px;
-  margin: 50px auto 0 auto;
+  margin-top: 50px;
 `;
